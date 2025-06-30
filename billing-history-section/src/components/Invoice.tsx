70d0c14f9c9d0e9144e865693c826e5ab40b7b3a
owinogabriel <g73346920@gmail.com>
2025-06-30 23:59:11 +0300

    feat: center content, left-align text, and reduce table border size

--- billing-history-section/src/components/Invoice.tsx
+++ billing-history-section/src/components/Invoice.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Invoice = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-[#ffffff]">
-      <div className=" w-full text-center">
-        <h1 className="text-3xl text-black font-bold">Payment History</h1>
-        <p className="text-2xl text-gray-500 mt-2">
-          Please reach out to our friendly team via <span className="text-blue-500">team@codepulse.com</span> if you have questions.
+      <div className="w-full max-w-4xl text-center p-4">
+        <h1 className="text-2xl text-black font-bold text-left">
+          Payment History
+        </h1>
+        <p className="text-sm text-gray-500 mt-2 text-left">
+          Please reach out to our friendly team via{" "}
+          <span className="text-blue-500">team@codepulse.com</span> if you have
+          questions.
         </p>
-      
-        <div className="overflow-x-auto">
-          <table className=" text-sm text-left border">
-            <thead className=" text-gray-400">
-              <tr>
-                <th>Invoice</th>
-                <th>Status</th>
-                <th>Amount</th>
-                <th>Plan</th>
+
+        <div className="overflow-x-aut mt-6">
+          <table className="w-full md:w-[60rem] text-sm text-left border-slate-950 border mx-auto">
+            <thead className="text-black  font-extralight px-4">
+              <tr className="text-sm">
+                <th className="px-4 py-2">Invoice</th>
+                <th className="px-4 py-2">Status</th>
+                <th className="px-4 py-2">Amount</th>
+                <th className="px-4 py-2">Plan</th>
               </tr>
             </thead>
           </table>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Invoice
+export default Invoice;
