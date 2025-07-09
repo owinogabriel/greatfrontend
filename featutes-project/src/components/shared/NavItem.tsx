a9657fc3ebdf380e0082f7cace2181f95f8b552d
owinogabriel <g73346920@gmail.com>
2025-07-09 23:50:55 +0300

    feat:defined the layout of the nav list

---
+++ featutes-project/src/components/shared/NavItem.tsx
@@ -0,0 +1,15 @@
+
+interface NavItemProps {
+  href: string;
+  text: string;
+}
+
+export const NavItem = ({ href, text }: NavItemProps) => {
+  return (
+    <li>
+      <a href={href} className="duration-300 font-medium text-gray-500 ease-linear hover:text-primary py-3">
+        {text}
+      </a>
+    </li>
+  )
+}
