14f582d75b2b221cc30f2758d8694469efa26315
owinogabriel <g73346920@gmail.com>
2025-07-09 23:56:23 +0300

    feat:modified

--- featutes-project/src/components/shared/NavItem.tsx
+++ featutes-project/src/components/shared/NavItem.tsx
@@ -7,7 +7,7 @@ interface NavItemProps {
 export const NavItem = ({ href, text }: NavItemProps) => {
   return (
     <li>
-      <a href={href} className="duration-300 font-medium text-gray-500 ease-linear hover:text-primary py-3">
+      <a href={href} className="duration-300 font-semibold text-gray-500 ease-linear hover:text-primary py-3">
         {text}
       </a>
     </li>
