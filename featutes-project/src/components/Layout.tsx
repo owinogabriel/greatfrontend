7d3ea8797bca7fe293125c7c81740ab98e3d65f9
owinogabriel <g73346920@gmail.com>
2025-07-09 23:49:39 +0300

    feat:define the layout using the interface.

--- featutes-project/src/components/Layout.tsx
+++ featutes-project/src/components/Layout.tsx
@@ -1,1 +1,26 @@
+import {  useEffect} from "react";
+import { Navbar } from "./elements/Navbar";
+import  Footer  from "./elements/Footer";
 
+
+// Defining Layout
+interface LayoutProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export const Layout = ({ title, children }: LayoutProps) => {
+  //useEffect rendered for web title display when onmount
+  useEffect(() => {
+    document.title = title
+  }, [title])
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-col gap-y-20 md:gap-y-20 overflow-hidden">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+}
