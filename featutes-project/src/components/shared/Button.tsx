b132a50d88b1bdec397f53fe3be1b2e3b5651e91
owinogabriel <g73346920@gmail.com>
2025-07-10 14:20:09 +0300

    feat:Initial commit

---
+++ featutes-project/src/components/shared/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+  children: string;
+  className?: string;
+  onclick?: () => void;
+}
+
+export const Button = ({ children, className, onclick }: ButtonProps) => {
+  return (
+    <button
+      className={`font-semibold text-sm shadow-lg cursor-pointer px-6 py-2 rounded outline-none relative border-none overflow-hidden ${className}`}
+      onClick={onclick}
+    >
+      {children}
+    </button>
+  );
+};
