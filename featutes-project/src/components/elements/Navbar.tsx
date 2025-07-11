1d933e9020c7ce6605099f2c15149be9c507b70c
owinogabriel <g73346920@gmail.com>
2025-07-11 15:46:03 +0300

    feat:Image w set to full in big screens

--- featutes-project/src/components/elements/Navbar.tsx
+++ featutes-project/src/components/elements/Navbar.tsx
@@ -13,26 +13,25 @@ export const navItems = [
 
 export const Navbar = () => {
   return (
-    <header className="absolute inset-x-0  z-50 py-6">
+    <header className="absolute inset-x-0 z-50">
       {/* Full-width background wrapper */}
       <div className="w-full bg-gray-400">
         <Container className="">
-          <nav className="w-full flex justify-between gap-6 relative">
-            <div className="min-w-max inline-flex relative">
+          <nav className="w-full flex justify-center gap-6 md:gap-x-7 relative">
+            <div className="inline-flex relative">
               <a href="/" className="relative flex items-center gap-3">
-                <img src={logo} alt="abstract" className="w-full" />
+                <img src={logo} alt="abstract" className="w-32 sm:w-full" />
               </a>
             </div>
-
             <div
               className="flex flex-col lg:flex-row w-full lg:justify-between lg:items-center
                          absolute top-full left-0 lg:static lg:top-0 bg-body lg:bg-transparent
                          border-x border-x-box-border lg:border-x-0 lg:h-auto h-0 overflow-hidden"
             >
               <ul
-                className="border-t border-box-border lg:border-t-0 px-6 lg:px-0
+                className=" border-t border-box-border lg:border-t-0 px-6 lg:px-0
                            pt-6 lg:pt-0 flex flex-col lg:flex-row gap-y-6 gap-x-7
-                           text-lg text-heading-2 w-full lg:justify-center lg:items-center"
+                           text-lg text-heading-2 w-full lg:justify-center lg:items-center hover:text-amber-300"
               >
                 {navItems.map((item, index) => (
                   <NavItem href={item.href} text={item.text} key={index} />
