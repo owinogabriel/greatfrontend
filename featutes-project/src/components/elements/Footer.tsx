4863acb9bc79347429db62299f554be6f5f6bee1
owinogabriel <g73346920@gmail.com>
2025-07-11 15:47:12 +0300

    feat:Initial commit

--- featutes-project/src/components/elements/Footer.tsx
+++ featutes-project/src/components/elements/Footer.tsx
@@ -1,5 +1,19 @@
-const Footer = () => {
-  return <div></div>;
-};
+import { Container } from "../shared/Container";
+import { navItems } from "./Navbar";
+import { NavItem } from "../shared/NavItem";
 
-export default Footer;
+export const Footer = () => {
+  return (
+    <footer className="relative pt-28 rounded-t-3xl bg-box-bg">
+      <Container className="pb-8">
+        <div className="flex flex-col  md:flex-row justify-center items-center">
+          <ul className="flex gap-6">
+            {navItems.map((item, key) => (
+              <NavItem key={key} href={item.href} text={item.text} />
+            ))}
+          </ul>
+        </div>
+      </Container>
+    </footer>
+  );
+};
