44f7a610beb6a425818bd847f1668590c02ff49e
owinogabriel <g73346920@gmail.com>
2025-07-11 19:47:40 +0300

    feat:text color changed

--- featutes-project/src/components/shared/NavItem.tsx
+++ featutes-project/src/components/shared/NavItem.tsx
@@ -8,7 +8,7 @@ export const NavItem = ({ href, text }: NavItemProps) => {
     <li>
       <a
         href={href}
-        className="duration-300 font-semibold text-[10px] md:text-sm text-gray-300 ease-linear cursor-pointer hover:text-amber-100 py-3"
+        className="duration-300 font-semibold text-[10px] md:text-sm text-[#BCBCBC] ease-linear cursor-pointer hover:text-amber-100 py-3"
       >
         {text}
       </a>
