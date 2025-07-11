b64e74a20154109b2fa8ddda9e59f6ec49b88c01
owinogabriel <g73346920@gmail.com>
2025-07-11 16:16:22 +0300

    feat:Initial commit

--- featutes-project/src/components/elements/Footer.tsx
+++ featutes-project/src/components/elements/Footer.tsx
@@ -6,12 +6,15 @@ export const Footer = () => {
   return (
     <footer className="relative pt-28 rounded-t-3xl bg-box-bg">
       <Container className="pb-8">
-        <div className="flex flex-col  md:flex-row justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
           <ul className="flex gap-6">
             {navItems.map((item, key) => (
               <NavItem key={key} href={item.href} text={item.text} />
             ))}
           </ul>
+            <p className="text-sm font-medium md:text-lg md:mt-6">
+              © 2025 Abstractly,Inc. All rights reserved.
+            </p>
         </div>
       </Container>
     </footer>
