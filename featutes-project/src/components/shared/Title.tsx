f3769cc795f9d9ff4987cef364686c6dc4017bf8
owinogabriel <g73346920@gmail.com>
2025-07-14 15:45:53 +0300

    feat:intial commit

---
+++ featutes-project/src/components/shared/Title.tsx
@@ -0,0 +1,23 @@
+
+interface TitleProps{
+  children:React.ReactNode;
+  className?:string;
+}
+
+
+interface SubTitleProps{
+  children:React.ReactNode;
+  className?:string;
+}
+
+export  const Title = ({children, className=""}:TitleProps) => {
+return(
+  <h1 className={`text-[#554CCD] font-semibold text-sm ${className}`} >{children}</h1>
+)
+}
+
+export const SubTitle = ({children, className =""}: SubTitleProps) => {
+  return(
+    <h2 className={`text-3xl  text-[#171717] font-semibold text-center md:text-4xl ${className}`} >{children}</h2>
+  )
+}
