22896f64e57dea2180066de28a782240a0cc094c
owinogabriel <g73346920@gmail.com>
2025-07-14 15:47:00 +0300

    feat:import the Premuim file module.

--- featutes-project/src/App.tsx
+++ featutes-project/src/App.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "./components/Layout";
 import Hero from "./components/sections/Hero";
+import Premium from "./components/sections/Premium";
 
 function App() {
   return (
     <Layout title="Feature | Abstractly">
       <Hero />
+      <Premium />
     </Layout>
   );
 }
