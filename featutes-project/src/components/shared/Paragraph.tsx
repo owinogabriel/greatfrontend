ca1c4fcdd92472d1748d48ae26ed8f4d6349adad
owinogabriel <g73346920@gmail.com>
2025-07-14 15:55:03 +0300

    feat:update the paragraph text size increase on big screens

--- featutes-project/src/components/shared/Paragraph.tsx
+++ featutes-project/src/components/shared/Paragraph.tsx
@@ -1,11 +1,11 @@
-interface ParagraphProp {
+interface ParagraphProps {
   children: React.ReactNode;
   className?: string;
 }
 
-export const Paragraph = ({ children, className = "" }: ParagraphProp) => {
+export const Paragraph = ({ children, className = "" }: ParagraphProps) => {
   return (  
-    <p className={`text-2xl justify-center text-center text-[#828282] md:text-lg ${className}`}>
+    <p className={`text-2xl justify-center text-center text-[#828282] md:text-3xl pt-2 ${className}`}>
       {children}
     </p>
   )
