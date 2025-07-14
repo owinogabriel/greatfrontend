ab2ef1a7bd7a53988bd015a2254dfc998fc2a01e
owinogabriel <g73346920@gmail.com>
2025-07-14 15:47:36 +0300

    feat:update document formatted with prettier.

--- featutes-project/src/components/sections/Premium.tsx
+++ featutes-project/src/components/sections/Premium.tsx
@@ -1,4 +1,3 @@
-
 import { Container } from "../shared/Container";
 import { Paragraph } from "../shared/Paragraph";
 import { SubTitle, Title } from "../shared/Title";
@@ -10,7 +9,10 @@ const Premium = () => {
         <div className="flex flex-col items-center justify-center gap-3">
           <Title>Premium abstract images</Title>
           <SubTitle>Easy access to top quality images</SubTitle>
-          <Paragraph>In a world where storytelling constantly evolves, we lead with groundbreaking images designed for your presentattion excellence.</Paragraph>
+          <Paragraph>
+            In a world where storytelling constantly evolves, we lead with
+            groundbreaking images designed for your presentattion excellence.
+          </Paragraph>
         </div>
       </Container>
     </div>
