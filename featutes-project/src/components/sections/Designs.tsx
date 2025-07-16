07f80dfb6ea113f51120b02007b1ba806c0be7a9
owinogabriel <g73346920@gmail.com>
2025-07-16 23:14:05 +0300

    refactor: restructure layout to display features array on left and image on right side.

---
+++ featutes-project/src/components/sections/Designs.tsx
@@ -0,0 +1,62 @@
+import { Container } from "../shared/Container";
+import { Paragraph } from "../shared/Paragraph";
+import { SubTitle, Title } from "../shared/Title";
+import design from "/assets/images/design.jpg";
+const features = [
+  {
+    text: "5K resolution support",
+    description:
+      "All images boast a minimum resolution of 5K, ensuring crisp, crystal-clear quality.",
+  },
+  {
+    text: "From water to glass",
+    description:
+      "We offer a wide array of abstractions, ranging from water to glass and encompassing various styles including 3D and vector.",
+  },
+  {
+    text: "Portrait or lanscape",
+    description:
+      "Effortlessly adapt you images for any platform - whether it's a stunning wallpaper or captivation Instagram reels and stories.",
+  },
+];
+
+const Designs = () => {
+  return (
+    <div>
+      <Container>
+        <div className="flex flex-col items-center justify-center gap-3">
+          <Title>High quality images</Title>
+          <SubTitle>For designers, by designers</SubTitle>
+          <Paragraph className="font-medium">
+            Unleash boundless creativity with a large repository of images
+            optimized for designers
+          </Paragraph>
+          <div className="flex flex-col md:flex-row gap-6 mt-5">
+            <div className="flex-2 flex flex-col gap-4">
+              {features.map((feature, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col text-start justify-start"
+                >
+                  <h2 className="text-lg font-semibold mt-4">{feature.text}</h2>
+                  <p className="mt-2 max-w-[50ch] text-[#828282]">
+                    {feature.description}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <div className="flex-2 flex items-start justify-center pt-">
+              <img
+                src={design}
+                alt="Design abstract"
+                className="w-full h-40 md:h-[30%] object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Designs;
