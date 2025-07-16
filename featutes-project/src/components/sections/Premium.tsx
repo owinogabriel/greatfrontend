13b104d179bcf00abf500d520c9b60267249354c
owinogabriel <g73346920@gmail.com>
2025-07-16 12:54:46 +0300

    feat:updated the define arrays

--- featutes-project/src/components/sections/Premium.tsx
+++ featutes-project/src/components/sections/Premium.tsx
@@ -2,6 +2,36 @@ import { Container } from "../shared/Container";
 import { Paragraph } from "../shared/Paragraph";
 import { SubTitle, Title } from "../shared/Title";
 
+const abstract: {
+  text: string;
+  description: string;
+}[] = [
+  {
+    text: "Infinite Download",
+    description:
+      "Once you subscribe to our plans, they're all yours. Download as many as you want and use them for work presentations, wallpapers, and much more.",
+  },
+  {
+    text: "Purely Handcrafted",
+    description:
+      "NO AI, no generic images. Crafted from various chemical, fabrics, clouds, or even particles as small as dust.",
+  },
+  {
+    text: "All are under licensed",
+    description:
+      "The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy(such as painting or prints on papy).",
+  },
+  {
+    text: "Empowering For Team",
+    description:
+      "We support multiples seats at once, requiring only a single payment.",
+  },
+  {
+    text: "No Limitations",
+    description:
+      "Use as many as you want, from Dribble presentation to PowerPoint presantations.",
+  },
+];
 const Premium = () => {
   return (
     <div>
