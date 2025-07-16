977a195e4d3de26bcb4959d3c8d91df928cf2e17
owinogabriel <g73346920@gmail.com>
2025-07-16 13:43:54 +0300

    feat:INitial commits

--- featutes-project/src/components/sections/Premium.tsx
+++ featutes-project/src/components/sections/Premium.tsx
@@ -1,7 +1,9 @@
+// Import shared components
 import { Container } from "../shared/Container";
 import { Paragraph } from "../shared/Paragraph";
 import { SubTitle, Title } from "../shared/Title";
 
+// Data array containing abstract image features
 const abstract: {
   text: string;
   description: string;
@@ -22,6 +24,10 @@ const abstract: {
       "The only limitation with these abstract images is that you are not able to sell them in any form, whether digital or hard copy(such as painting or prints on papy).",
   },
   {
+    text:"Cancel Anytime",
+    description:"Subscribe at your own pace, and cancel when you feel it's enough."
+  },
+  {
     text: "Empowering For Team",
     description:
       "We support multiples seats at once, requiring only a single payment.",
@@ -33,16 +39,20 @@ const abstract: {
   },
 ];
 
+// defining props for AbstractGrid component
 interface AbstractProps {
   title: string;
   description: string;
 }
 
+// Individual grid item component for displaying feature cards
 const AbstractGrid = ({ title, description }: AbstractProps) => {
   return (
-    <div className="w-full max-w-2xl grid grid-cols-1 mt-7 text-justify pt-5">
-      <SubTitle>{title}</SubTitle>
-      <Paragraph className="text-xm max-w-[20cv] mt-4">{description}</Paragraph>
+    <div className="text-center pt-4 flex flex-col items-center ">
+      {/* Feature title */}
+      <h1 className="text-lg mt-2.5 font-medium md:text-2xl">{title}</h1>
+      {/* Feature description with text styling and character width limit */}
+      <p className="text-xm md:text-lg font-medium text-[#828282] w-full md:max-w-2xl mt-2">{description}</p>
     </div>
   );
 };
@@ -58,7 +68,8 @@ export const Premium = () => {
             In a world where storytelling constantly evolves, we lead with
             groundbreaking images designed for your presentattion excellence.
           </Paragraph>
-          <div>
+          
+          <div className="grid grid-cols-1 lg:grid-cols-3 mt-4 md:gap-[4rem] w-full max-w-6xl">
             {abstract.map((abstracts, index) => (
               <AbstractGrid
                 key={index}
@@ -72,3 +83,5 @@ export const Premium = () => {
     </div>
   );
 };
+
+export default Premium;
