bcc437174b45edc8e4d97770b38539b8170a557e
owinogabriel <g73346920@gmail.com>
2025-07-16 22:25:25 +0300

    feat:Modified

--- featutes-project/src/components/shared/Paragraph.tsx
+++ featutes-project/src/components/shared/Paragraph.tsx
@@ -1,9 +1,11 @@
 import type { ParagraphProps } from "../../interface";
 
 export const Paragraph = ({ children, className = "" }: ParagraphProps) => {
-  return (  
-    <p className={`text-2xl justify-center text-center text-[#828282] md:text-3xl pt-2 ${className}`}>
+  return (
+    <p
+      className={`text-sm justify-center text-center max-w-2xl text-[#828282] md:text-lg pt-3 ${className}`}
+    >
       {children}
     </p>
-  )
+  );
 };
