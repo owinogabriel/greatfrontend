2d905dd5bcd178bb5b706e80782eca0f8e719dfb
owinogabriel <g73346920@gmail.com>
2025-07-21 23:05:45 +0300

    feat:updated the faq data

--- featutes-project/src/components/sections/Testimonials.tsx
+++ featutes-project/src/components/sections/Testimonials.tsx
@@ -3,6 +3,7 @@ import { Paragraph } from "../shared/Paragraph";
 import SharedCard from "../shared/SharedCard";
 import { SubTitle, Title } from "../shared/Title";
 
+// Testiomials
 const testimonialData = [
   {
     avatar: "/assets/images/sarah.jpg",
@@ -68,6 +69,34 @@ const testimonialData = [
       "The customer support team went above and beyond to help me with a subscription issue. Their dedication to customer satisfaction truly commendable.",
   },
 ];
+
+// frequent asked questions (faqs)
+const faqs = [
+  {
+    question:"What types of images are available on your platform?",
+    answer:"Our platform offers a diverse range of abstract images to suit various preference and needs. From vibrant geometric patterns to soothing landscapes, we strive to provide a wide selection to cater to different tastes."
+  },
+  {
+    question:"How can I access and download images from your platform?",
+    answer:"Accessing and downloading images from our platform is simple. Upon signing up and logging in, users can browse through our curated collection and download their chosen images directly to their devices with just a few clicks."
+  },
+  {
+    question:"Do you offer free images, or is there a subscription required?",
+    answer:"We provide both free and premium images on our platform, Users can explore a selection of free images without any subscription. For accessto our entire library and additional features, we offer subscription plans tailored to different user needs."
+  },
+  {
+    question:"What payment methods do you accept for subscription?",
+    answer:"We accept a variety of payment methods, including credit/debit cards and online payment gateways, to make the subscription process convenient for our users."
+  },
+  {
+    question:"Can I cancel or modify my subscription at anytime?",
+    answers:"Yes, absolutely, You have the flexibility to cancel or modify your subscription at any time through your account settings. Changes will take effect immediately, ensuring you have full control over your subscription preferences."
+  },
+  {
+    question:"How frequently do you update your image collection?",
+    answers:"We regulerly update our image collection with frsh and captivating content to keep our users inspired and engaged. New images are added consistently to ensure there's always something new to discover on our platform."
+  },
+]
 
 const Testimonials = () => {
   return (
