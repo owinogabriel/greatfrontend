b25fc8b24115cba97ba0e8d76f40921de0b626a8
owinogabriel <g73346920@gmail.com>
2025-07-25 18:59:01 +0300

    feat:styled the small  devices.

--- featutes-project/src/components/sections/Contact.tsx
+++ featutes-project/src/components/sections/Contact.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../shared/Button";
 import { Container } from "../shared/Container";
 import { Paragraph } from "../shared/Paragraph";
 import { SubTitle } from "../shared/Title";
@@ -20,14 +21,45 @@ const Contact = () => {
                 <Paragraph className="text-start  max-w-[20ch]">
                   123 Maples Street, Springfield, IL, USA
                 </Paragraph>
-                <Paragraph className="text-start ">
-                  +1 (650) 555-0198
-                </Paragraph>
+                <Paragraph className="text-start ">+1 (650) 555-0198</Paragraph>
                 <Paragraph className="text-start">
                   hello@abstractly.com
                 </Paragraph>
               </div>
             </div>
+          </div>
+          <div>
+            <form
+              className="bg-[#ffffff] space-y-6 mt-6 p-8 rounded shadow-md shadow-gray-500 w-full "
+              noValidate
+            >
+              <div>
+                <label htmlFor="text">Name</label>
+                <input
+                  type="name"
+                  placeholder="Your Name"
+                  className="w-full p-3 border border-gray-400 rounded bg-[#f1f4fa] text-gray-950 placeholder:text-gray-500"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  placeholder="Your Email"
+                  className="w-full p-3 border border-gray-400 rounded bg-[#f1f4fa] text-gray-950 placeholder:text-gray-500"
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email</label>
+                <textarea
+                  rows={5}
+                  placeholder="Your Email"
+                  className="w-full p-3 border border-gray-400 rounded bg-[#f1f4fa] text-gray-950 placeholder:text-gray-500"
+                />
+              </div>
+              <Button className="w-full mt-4 bg-[#4e45cf] text-[#FFFFFF]">Submit</Button>
+            </form>
           </div>
         </div>
       </Container>
